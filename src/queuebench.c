4bf25a1e4598110d195290d72d9dc0cd15520335
Frank Quinn <fquinn.ni@gmail.com>
2016-06-05 16:46:31 +0100

    Updated queue benchmarking script with reordering on startup
    Reordering was required to prevent race condition on startup causing
    an early gap from time to time.

--- src/queuebench.c
+++ src/queuebench.c
@@ -4,8 +4,10 @@
 #include <string.h>
 #include <wombat/port.h>
 #include <wombat/queue.h>
+#include <wombat/wInterlocked.h>
 
 #define QB_SIZE 10000000
+#define QB_MAX_CONS 32
 
 #define QUEUE_WOMBAT 1
 #define QUEUE_ZMQ    2
@@ -13,7 +15,8 @@
 struct qbn {
     char mUri[256];
     wombatQueue mWombatQueue;
-    int mReceived;
+    int mNumSubs;
+    int mNumPubs;
 };
 
 struct qbnEndpoint {
@@ -25,34 +28,41 @@ struct qbnEndpoint {
 void* gCtx = NULL;
 int gDebug = 0;
 int gQueueChoice = 0;
+wInterlockedInt gSentEvents;
+wInterlockedInt gReceivedEvents;
 
 void wQueueCb (void* data, void* closure)
 {
-    struct qbn* newQbn = (struct qbn*) data;
-    newQbn->mReceived++;
+    wInterlocked_increment (&gReceivedEvents);
 }
 
 void* dealer (void* closure)
 {
     struct qbnEndpoint* ep = (struct qbnEndpoint*) closure;
     struct qbn* newQbn = ep->mQbn;
-    long int i = 0;
     int hwm = 0;
+    int currentSize = 0;
+    int local = 0;
 
-    for (i = 0; i < QB_SIZE*10; i++)
+    while ((currentSize = wInterlocked_read (&gSentEvents)) <= QB_SIZE)
     {
         if (gQueueChoice == QUEUE_ZMQ) {
-            char buf[2] = {'A', '\0'};
-            int rc = zmq_send (ep->mSocket, strdup(buf), sizeof(buf), 0);
-            //printf ("SENT %d [%d]\n", rc, errno);
+            char buf[6];
+            buf[0] = 'A';
+            buf[1] = '\0';
+
+            memcpy (buf+2, &currentSize, sizeof(int));
+            zmq_send (ep->mSocket, buf, 6, 0);
         }
         if (gQueueChoice == QUEUE_WOMBAT) {
-            wombatQueue_enqueue (newQbn->mWombatQueue, wQueueCb, newQbn, NULL);
+            wombatQueue_enqueue (newQbn->mWombatQueue, wQueueCb, NULL, NULL);
         }
-        //if (i % 100000 == 0)
-        //    printf ("So far spammed %ld messages onto queue %d\n", i, ep->mIdx);
+        local++;
+        if (currentSize % 1000000 == 0)
+            printf ("So far spammed %ld messages onto queue %d\n", currentSize, ep->mIdx);
+        wInterlocked_increment (&gSentEvents);
     }
-    printf ("Finished spamming %ld messages onto queue\n");
+    printf ("Done spamming from producer %d (%d messages from here)\n", ep->mIdx, local);
 }
 
 void* worker (void* closure)
@@ -60,38 +70,57 @@ void* worker (void* closure)
     struct qbnEndpoint* ep = (struct qbnEndpoint*) closure;
     struct qbn* newQbn = ep->mQbn;
     wombatQueueStatus status;
+    int hwm = 0;
+    int rcvto = 10;
+
     zmq_msg_t zmsg;
     zmq_msg_init (&zmsg);
 
-    while (newQbn->mReceived < QB_SIZE)
+    int lastSeqNo = 0;
+    int recvSeqNo = 0;
+    int recEv = 0;
+    int local = 0;
+    while ((recEv = wInterlocked_read (&gReceivedEvents)) < QB_SIZE)
     {
         if (gQueueChoice == QUEUE_ZMQ) {
-            int size = zmq_msg_recv (&zmsg, ep->mSocket, ZMQ_DONTWAIT);
+            int size = zmq_msg_recv (&zmsg, ep->mSocket, 0);
             if (size == -1)
             {
+                printf ("LOCALLY RECEIVED %d in queue %d\n", local, ep->mIdx);
+                printf ("Timing out waiting for message...\n");
                 continue;
             }
-            newQbn->mReceived++;
+
+            memcpy (&recvSeqNo, ((char*)zmq_msg_data(&zmsg)) + 2, sizeof(int));
+//            if (ep->mQbn->mNumSubs == 1 && recvSeqNo > 0 && recvSeqNo != lastSeqNo + 1)
+//                printf ("Gap detected - %d != %d\n", recvSeqNo, lastSeqNo + 1);
+
+            wInterlocked_increment (&gReceivedEvents);
+            lastSeqNo = recvSeqNo;
+            local++;
         }
         if (gQueueChoice == QUEUE_WOMBAT) {
-            status = wombatQueue_timedDispatch (newQbn->mWombatQueue, NULL, NULL, 1);
-            if (WOMBAT_QUEUE_TIMEOUT == status)
-                continue;
+            status = wombatQueue_timedDispatch (newQbn->mWombatQueue, NULL, NULL, 1000);
         }
-        //if (newQbn->mReceived % 100000 == 0)
-        //    printf ("So far chewed %ld messages from queue %d\n", newQbn->mReceived, ep->mIdx);
-
+        if (recEv > 0 && recEv % 1000000 == 0)
+            printf ("So far chewed %d messages from queue %d\n", recEv, ep->mIdx);
     }
-
 }
 
 int main (int argc, char *argv[])
 {
     int i = 0;
     int hwm = 0;
-    int rcvto = 10;
+    int rcvto = 100;
     int pubs = 1;
     int subs = 1;
+
+    /* Create the counter lock. */
+    wInterlocked_initialize(&gSentEvents);
+    wInterlocked_set(0, &gSentEvents);
+
+    wInterlocked_initialize(&gReceivedEvents);
+    wInterlocked_set(0, &gReceivedEvents);
 
     if (argc == 1 || argc > 4) {
         printf ("Usage: queuebench [wombat|zmq] [producers] [consumers]");
@@ -111,8 +140,12 @@ int main (int argc, char *argv[])
     subs = atoi(argv[3]);
 
     struct qbn* newQbn = calloc(1, sizeof(struct qbn));
+    newQbn->mNumSubs = subs;
+    newQbn->mNumPubs = pubs;
     wthread_t* subThreads = calloc (subs, sizeof(wthread_t));
     wthread_t* pubThreads = calloc (pubs, sizeof(wthread_t));
+    struct qbnEndpoint* subEps = calloc (subs, sizeof(struct qbnEndpoint));
+    struct qbnEndpoint* pubEps = calloc (pubs, sizeof(struct qbnEndpoint));
 
     if (gQueueChoice == QUEUE_ZMQ) {
         gCtx = zmq_ctx_new ();
@@ -129,32 +162,57 @@ int main (int argc, char *argv[])
 
     for (i = 0; i < pubs; i++)
     {
-        struct qbnEndpoint* ep = calloc(1, sizeof(struct qbnEndpoint));
+        struct qbnEndpoint* ep = &pubEps[i];
+        ep->mIdx = i;
+        ep->mQbn = newQbn;
         void* dealerSocket = zmq_socket (gCtx, ZMQ_PUB);
         zmq_setsockopt (dealerSocket, ZMQ_SNDHWM, &hwm, sizeof(int));
-        zmq_bind (dealerSocket, newQbn->mUri);
-        ep->mIdx = i;
+        if (0 == i)
+        {
+            zmq_bind (dealerSocket, newQbn->mUri);
+        }
+        else
+        {
+            zmq_connect (dealerSocket, newQbn->mUri);
+        }
+        printf ("ERRNO for pub %d = %d\n", i, errno);
         ep->mSocket = dealerSocket;
-        ep->mQbn = newQbn;
-        wthread_create (&pubThreads[i], NULL, dealer, ep);
+        sleep (1);
     }
 
     for (i = 0; i < subs; i++)
     {
-        struct qbnEndpoint* ep = calloc(1, sizeof(struct qbnEndpoint));
+        struct qbnEndpoint* ep = &subEps[i];
+        ep->mIdx = i;
+        ep->mQbn = newQbn;
         void* workerSocket = zmq_socket (gCtx, ZMQ_SUB);
         zmq_setsockopt (workerSocket, ZMQ_RCVHWM, &hwm, sizeof(int));
         zmq_setsockopt (workerSocket, ZMQ_RCVTIMEO, &rcvto, sizeof(int));
         zmq_setsockopt (workerSocket, ZMQ_SUBSCRIBE, "A", 1);
         zmq_connect (workerSocket, newQbn->mUri);
-        ep->mIdx = i;
         ep->mSocket = workerSocket;
-        ep->mQbn = newQbn;
-        wthread_create (&subThreads[i], NULL, worker, ep);
+
     }
 
     for (i = 0; i < subs; i++)
+    {
+        wthread_create (&subThreads[i], NULL, worker, &subEps[i]);
+    }
+
+    for (i = 0; i < pubs; i++)
+    {
+        wthread_create (&pubThreads[i], NULL, dealer, &pubEps[i]);
+    }
+
+    for (i = 0; i < subs; i++)
+    {
         wthread_join (subThreads[i], NULL);
+    }
+
+    for (i = 0; i < pubs; i++)
+    {
+        wthread_join (pubThreads[i], NULL);
+    }
 
     printf ("Finished chewing through %ld messages across %d producers and %d consumers\n", QB_SIZE, pubs, subs);
 }
