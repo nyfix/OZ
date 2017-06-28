0dbfc1b825b4ef6b1eadba1611566667e00ff3c9
btorpey <btorpey@btmacpro.ullink.lan>
2017-06-28 10:28:16 -0400

    keep a copy of potential detach fixes

--- src/msg.c
+++ src/msg.c
@@ -688,34 +688,19 @@ zmqBridgeMamaMsgImpl_deserialize (msgBridge        msg,
               impl->mMsgType);
 
     mama_status status;
-#if 0
     // if the message already has a payload, we need to destroy the old one before
     // replacing it
     // to do that, we force msg's owner flag to 1 prior to calling setMsgBuffer
     // (setMsgBuffer sets the owner flag to 0)
-    msgPayload payload = NULL;
-    status = mamaMsgImpl_getPayload(impl->mParent, &payload);
-    if (payload != NULL) {
-       mamaMsgImpl_setMessageOwner(impl->mParent, 1);
-    }
-#endif
 
     status = mamaMsgImpl_setMsgBuffer (target,
                                        (void*) bufferPos,
                                        payloadSize,
                                        *bufferPos);
 
-    #if 0
     // let's not forget metadata
-    if (MAMA_STATUS_OK == status) {
-       mamaMsgReply handle;
-       mamaMsg_getReplyHandle (target, &handle);
-       handle->replyHandle = &impl->mReplyHandle;
        // resolves to zmqBridgeMamaMsgImpl_setReplyHandle(), which copies its input params,
        // so no need to worry about lifetimes here
-       status = mamaMsg_setReplyHandle(target, handle);
-    }
-    #endif
 
     return status;
 }
