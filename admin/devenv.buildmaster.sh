76c3a94edb3bd1124c2c8df65f3d1601fe36e7cc
wtorpey <bill.torpey@ullink.com>
2018-09-06 16:17:12 -0400

    - allow for building w/system compiler on RH7

--- admin/devenv.buildmaster.sh
+++ admin/devenv.buildmaster.sh
@@ -14,10 +14,17 @@ unset MALLOC_CHECK_
 # standard install location
 export INSTALL_BASE=/build/share
 
-# use "nyfix" gcc
-export GCC_ROOT=/opt/nyfix/gcc/4.3.3
-export CC=${GCC_ROOT}/bin/gcc
-export CXX=${GCC_ROOT}/bin/g++
+# compiler depends on OS version
+OS=$(uname -r | sed 's/^.*\(el[0-9]\+\).*$/\1/')
+if [[ "${OS}" == "el5" ]]; then
+   # use "nyfix" gcc
+   export GCC_ROOT=/opt/nyfix/gcc/4.3.3
+   export CC=${GCC_ROOT}/bin/gcc
+   export CXX=${GCC_ROOT}/bin/g++
+else
+   export CC=$(which gcc)
+   export CXX=$(which g++)
+fi
 
 # needed to prevent multiple definition errors with -std=gnu99" (see BUS-1798)
 export CFLAGS=-fgnu89-inline
