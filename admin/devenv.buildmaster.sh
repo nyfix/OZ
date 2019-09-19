f115dd2f065021001940958d27689dfd752d1d52
Bill Torpey <bill.torpey@itiviti.com>
2019-09-19 14:20:10 -0400

    [BUS-1863] - release target is CentOS 7 w/gcc 4.8.5

--- admin/devenv.buildmaster.sh
+++ admin/devenv.buildmaster.sh
@@ -16,24 +16,10 @@ OPENMAMA_INSTALL=${INSTALL_BASE}/OpenMAMA/${OPENMAMA_VERSION}/${BUILD_TYPE}
 OPENMAMA_SOURCE=${OPENMAMA_INSTALL}/src
 LIBZMQ_INSTALL=${INSTALL_BASE}/libzmq/${LIBZMQ_VERSION}/${BUILD_TYPE}
 
-# build depends on OS version
-OS=$(uname -r | sed 's/^.*\(el[0-9]\+\).*$/\1/')
-if [[ "${OS}" == "el5" ]]; then
-
-   # need cmake for RH5
-   export PATH=/build/share/cmake/2.8.12.2/bin:$PATH
-
-   # use "nyfix" gcc
-   export GCC_ROOT=/opt/nyfix/gcc/4.3.3
-   export CC=${GCC_ROOT}/bin/gcc
-   export CXX=${GCC_ROOT}/bin/g++
-else
-   export CC=$(which gcc)
-   export CXX=$(which g++)
+# use system default compiler (gcc 4.8.5 on el7)
+export CC=$(which gcc)
+export CXX=$(which g++)
 fi
-
-# needed to prevent multiple definition errors with -std=gnu99" (see BUS-1798)
-export CFLAGS=-fgnu89-inline
 
 # indicate to devenv.sh NOT to source devenv.common.sh
 return 255
