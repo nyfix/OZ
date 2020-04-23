e2fe37f2d625ee68943b7e93f523229ede3aae62
Bill Torpey <bill.torpey@itiviti.com>
2020-04-23 16:49:48 -0400

    update test scripts

--- test/consumer.sh
+++ test/consumer.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE}[0]) && pwd)
 source ${SCRIPT_DIR}/setenv.sh
 
 # uncomment following to run w/process pinned to a CPU
-#[[ ${BUILD_TYPE} == "release"  ]] && PREFIX="taskset -c 2"
+#PREFIX="taskset -c 2"
 
 ${PREFIX} `which mamaconsumerc_v2` -tport ${MAMA_TPORT_SUB} -m ${MAMA_MW} $*
