5b6a5e58eee177559b5b5536cc9aba983a4fecd7
Bill Torpey <bill.torpey@itiviti.com>
2020-04-24 10:34:42 -0400

    update test scripts

--- test/producer.sh
+++ test/producer.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE}[0]) && pwd)
 source ${SCRIPT_DIR}/setenv.sh
 
 # uncomment following to run w/process pinned to a CPU
-#[[ ${BUILD_TYPE} == "release"  ]] && PREFIX="taskset -c 3"
+#PREFIX="taskset -c 1"
 
 ${PREFIX} `which mamaproducerc_v2` -tport ${MAMA_TPORT_PUB} -m ${MAMA_MW} $*
