8561abe9d7da4db6bfdc026446cf72f57256a455
Bill Torpey <bill.torpey@itiviti.com>
2020-08-25 11:29:05 -0400

    update test script

--- test/setenv.sh
+++ test/setenv.sh
@@ -4,6 +4,6 @@
 export BUILD_ROOT=/tmp/buildtemp
 
 # change this to use a different install location
-export INSTALL_BASE=/tmp/oz
+export INSTALL_BASE=/tmp/oztemp
 export PATH=${INSTALL_BASE}/bin:$PATH
 export LD_LIBRARY_PATH=${INSTALL_BASE}/lib64:${INSTALL_BASE}/lib:$LD_LIBRARY_PATH
